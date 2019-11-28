c4e44137610dd4f0d3539ce73eb53871cfe1ea38
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2019-11-28 16:15:05 +0900

    add_message-function

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
+  mount_uploader :image, ImageUploader
   belongs_to :group
   belongs_to :user
+  with_options presence: true do
+    validates :text
+    validates :image
+  end
 end
