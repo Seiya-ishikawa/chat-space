a781f7c54bb8447b86da9255c5778988629847f5
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2019-12-14 13:12:46 +0900

    add_delete_group-member_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
+  def index
+    return nil if params[:keyword] == ""
+    @users =User.where(['name LIKE ?', "%#{params[:keyword]}%"]).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
 def edit
   
 end
@@ -14,5 +24,6 @@ end
 private
 
 def user_params
-  params.require(:user).permit(:name, ;email)
+  params.require(:user).permit(:name, :email)
+end
 end
