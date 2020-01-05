9e38ab797a06bef84e3fbe1b686a8e3f6923c9da
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-01-05 18:11:44 +0900

    fix_error

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module ChatSpace
       g.test_framework false
       config.i18n.default_locale = :ja
       config.time_zone = 'Asia/Tokyo'
-      config.active_record.default_timezone = :utc
+      config.active_record.default_timezone = :local
     end
   end
 end
